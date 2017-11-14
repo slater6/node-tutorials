755a8b42e4d52481eccd7abd9f280d36226986c5
Bryan <slaters@gmail.com>
2017-11-14 14:20:57 -0500

    Setup socket.io/node chat app tutorial

---
+++ chat-app-tutorial/server.js
@@ -0,0 +1,56 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+
+mongo.connect('mongodb://slater:nirvana@ds259085.mlab.com:59085/chat-app', function(err,db){
+    if(err){
+        throw err;
+    }
+
+    client.on('connection',function(socket){
+        let chat = db.collection('chats');
+
+        sendStatus = function(s){
+            socket.emit('status',s);
+        }
+
+        chat.find().limit(100).sort({_id:1}).toArray(function(err,res){
+            if(err){
+                throw err;
+            }
+
+            socket.emit('output', res);
+        });
+
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+            
+            if(name == '' || message == ''){
+                sendStatus('Please enter a name and message');
+                return;
+            }
+
+            chat.insert({
+                name:name,
+                message:message
+            }, function(){
+                client.emit('output',[data]);
+
+                sendStatus({
+                    message: 'Message Sent',
+                    clear: true
+                })
+            })
+
+        })
+
+        socket.on('clear', function(data){
+            chat.remove({},function(){
+                socket.emit('cleared');
+            })
+        })
+    });
+
+    console.log('Mongo DB Connected')
+})
+
